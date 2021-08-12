6e616f14ce73954a116c8c74302c0fff469b1023
Lazzy <64617706+LazzyDevelopment@users.noreply.github.com>
2021-08-12 19:04:00 +0900

    Update ping.ts

--- src/Slash/general/ping.ts
+++ src/Slash/general/ping.ts
@@ -3,6 +3,7 @@ import { Slash } from '../../interfaces';
 export const slash: Slash = {
 	name: 'ping',
 	description: 'ping command',
+	options: [],
 	testOnly: false,
 	run: async (client, interaction, args) => {
 		interaction.followUp({ content: `${client.ws.ping}ms` });
